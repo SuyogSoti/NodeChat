19cf6a47b5c63123af2d91861d100e698de83440
Suyog Soti <ssoti2201@bvsd.org>
2016-03-25 23:16:14 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var fs = require('fs');
+var server = http.createServer(app).listen(8080);
+console.log("listening in port 8080");
+
+var io = require('socket.io')(server);
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", function(req, res) {
+    res.writeHead(200);
+    res.end(fs.readFileSync('index.html'));
+});
+
+io.on('connection', function(socket) {
+    // console.log(socket);
+    console.log("a socket has connected");
+});
